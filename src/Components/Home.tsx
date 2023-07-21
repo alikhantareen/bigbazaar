6ca812e6cc667681859e32ccbb1ad06fd12edb1d
alikhantareen <creditableboy@gmail.com>
2023-07-21 21:53:19 +0500

    navbar added

---
+++ src/Components/Home.tsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            Home screen
+        </div>
+    )
+}
+
+
+export default Home;
